2f96f73629b31ad6e73b7838ca19948f6be49988
Ogunsanya James <oyindamolajames@gmail.com>
2022-10-09 14:02:45 +0100

    completed authentication with auth0

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,8 +1,12 @@
+from crypt import methods
+from functools import wraps
 import os
 from flask import Flask, request, jsonify, abort
 from sqlalchemy import exc
 import json
 from flask_cors import CORS
+from urllib.request import urlopen
+from jose import jwt
 
 from .database.models import db_drop_and_create_all, setup_db, Drink
 from .auth.auth import AuthError, requires_auth
@@ -11,23 +15,157 @@ app = Flask(__name__)
 setup_db(app)
 CORS(app)
 
+# TODO: PUT IN DOT_ENV
+# Configuration
+AUTH0_DOMAIN = 'dev-26mg-05q.us.auth0.com'
+ALGORITHMS = ['RS256']
+API_AUDIENCE = 'coffee'
+
+'''
+AuthError Exception
+A standardized way to communicate auth failure modes
+'''
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
+ ## Auth Header
+def verify_decode_jwt(token):
+    # GET THE PUBLIC KEY FROM AUTH0
+    jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
+    jwks = json.loads(jsonurl.read())
+    
+    # GET THE DATA IN THE HEADER
+    unverified_header = jwt.get_unverified_header(token)
+    
+    # CHOOSE OUR KEY
+    rsa_key = {}
+    if 'kid' not in unverified_header:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization malformed.'
+        }, 401)
+
+    for key in jwks['keys']:
+        if key['kid'] == unverified_header['kid']:
+            rsa_key = {
+                'kty': key['kty'],
+                'kid': key['kid'],
+                'use': key['use'],
+                'n': key['n'],
+                'e': key['e']
+            }
+    
+    # Finally, verify!!!
+    if rsa_key:
+        try:
+            # USE THE KEY TO VALIDATE THE JWT
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms=ALGORITHMS,
+                audience=API_AUDIENCE,
+                issuer='https://' + AUTH0_DOMAIN + '/'
+            )
+
+            return payload
+
+        except jwt.ExpiredSignatureError:
+            raise AuthError({
+                'code': 'token_expired',
+                'description': 'Token expired.'
+            }, 401)
+
+        except jwt.JWTClaimsError:
+            raise AuthError({
+                'code': 'invalid_claims',
+                'description': 'Incorrect claims. Please, check the audience and issuer.'
+            }, 401)
+        except Exception:
+            raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to parse authentication token.'
+            }, 400)
+    raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to find the appropriate key.'
+            }, 400)       
+
+
+
 '''
 @TODO uncomment the following line to initialize the datbase
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 !! Running this funciton will add one
 '''
-# db_drop_and_create_all()
+#db_drop_and_create_all()
+
+def get_token_auth_header():
+    if 'Authorization' not in request.headers:
+        abort(401)
+    auth_header = request.headers['Authorization']
+    header_part = auth_header.split(' ')
+    if len(header_part) != 2:
+        abort(401)
+    elif header_part[0].lower() != 'bearer':
+        abort(401)    
+    return header_part[1]
+
+def requires_auth(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        jwt = get_token_auth_header() 
+        # print(jwt)
+        try:
+            payload = verify_decode_jwt(jwt)
+        except Exception as e:
+            print(e)
+            abort(401)    
+        return f(payload, *args, **kwargs)
+    return wrapper      
+
+def format_drinks(drinks):
+    result_drinks = {}
+    for drink in drinks:
+        result_drinks["id"] = drink.id
+        result_drinks["title"] = drink.title
+        #result_drinks["recipe"] = drink.recipe. 
+        {
+        "id": drink.id,
+         "title": drink.title
+        # "recipe": [
+        #     {
+        #         "color": drink.recipe[0],
+        #         "parts": drink.recipie[0].part
+        #     }
+        # ]
+    }
+    return result_drinks
+
 
 # ROUTES
 '''
-@TODO implement endpoint
+@ TODO: implement endpoint
     GET /drinks
         it should be a public endpoint
         it should contain only the drink.short() data representation
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+
+
+@app.route("/drinks", methods=['GET'])
+@requires_auth
+def get_drinks(payload):
+    print(payload)
+    drinks_res = Drink.query.all()
+    return jsonify({
+        'success': True,
+        'drinks': format_drinks(drinks_res),
+        'total_drinks': len(drinks_res)
+    })
 
 
 '''
